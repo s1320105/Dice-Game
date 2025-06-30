fe2dcf9f568d09470d0d7a774a7fbcce6a480053
TSUJINO Kouta <s1320105@sshsv212.u-aizu.ac.jp>
2025-07-01 08:05:09 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,14 +1,29 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class DiceGame {
     public static void main(String[] args) {
-        Random random = new Random();
-        System.out.println("Rolling dice...");
-        int die1 = random.nextInt(6) + 1;
-        int die2 = random.nextInt(6) + 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name?\n> ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+
+        Random rand = new Random();
+        int die1 = rand.nextInt(6) + 1;
+        int die2 = rand.nextInt(6) + 1;
         int total = die1 + die2;
+
+        System.out.println("Rolling dice...");
         System.out.println("Die 1: " + die1);
         System.out.println("Die 2: " + die2);
         System.out.println("Total value: " + total);
+
+        if (total > 7) {
+            System.out.println(name + " won!");
+        } else {
+            System.out.println(name + " lost!");
+        }
+
+        scanner.close();
     }
 }
